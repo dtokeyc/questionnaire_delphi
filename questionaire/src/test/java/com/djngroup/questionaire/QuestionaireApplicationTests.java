84a7000e82a2d11c47784a00846b0ae1ce38f610
DingJianan <ding_key@163.com>
2020-04-18 17:14:20 +0800

    第一版

---
+++ questionaire/src/test/java/com/djngroup/questionaire/QuestionaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djngroup.questionaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
